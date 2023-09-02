6a6b49c5995b16c13312f968385e8dc1273e486d
Jose Zaccaro <a.zaccaro42@gmail.com>
2023-09-02 10:08:00 -0300

    layoutMovie

--- src/pages/DetalleMovie.jsx
+++ src/pages/DetalleMovie.jsx
@@ -37,39 +37,27 @@ const DetalleMovie = () => {
     }, [])
 
     // console.log(episodes);
-    console.log(movie);
+    console.log(movie)
     // console.log(Array(serie.seasons).fill('-'));
 
     return (
-        <div className='bg-cover bg-top min-h-[85vh]' style={{ backgroundImage: `url("${movie.detailImage}")` }}>
-            <div className='w-full min-h-[85vh] bg-gradient-to-b from-transparent to-black' >
+        <div className={`w-100 text-white min-h-[75vh] p-4 flex gap-x-4 flex-col`}>
 
-                <div className='flex justify-between'>
-                    <div className='flex gap-4 items-center'>
-                        <img className='w-28' src="/assets/images/M.png" alt="logo" />
-                        <h2 className='text-white text-3xl font-medium'>{movie.name}</h2>
+
+            <div className={`w-full h-[85vh] relative bg-contain bg-no-repeat bg-center`} style={{ backgroundImage: `url("${movie.detailImage || movie.coverImage}")` }}>
+                <div className='bg-[rgba(0,0,0,0.7)] w-full h-full flex justify-end flex-col rounded-lg'>
+                    <div className={`w-full flex flex-col p-10 absolute top-4 left-4 h-full justify-between`}>
+                        <h2 className='text-6xl font-bold mb-10 text-red-600 uppercase underline-offset-8 underline'>{movie.nombre}</h2>
+                        <div className={`w-full flex flex-col`}>
+                            <p className='text-lg mb-2 text-gray-400'>{movie.genre}</p>
+                            <p className='text-xl w-96 text-[#ccc]'>{movie.description}</p>
+                        </div>
                     </div>
-                    <img onClick={() => navigate('/movies')} className='cursor-pointer w-8 h-8 mt-4 mr-4' src="https://static.vecteezy.com/system/resources/previews/021/815/794/original/cross-close-icon-free-png.png" alt="x" />
                 </div>
+            </div>
 
-                <div className='w-full px-4'>
-                    {/* <select onChange={onHandleChange} value={temporada}>
-                        {
-                            Array(movie.seasons).fill('-').map((number, index) =>
-                                (<option value={index + 1} key={index}>Temporada {index + 1}</option>)
-                            )
-                        }
+            <div className='w-100 min-h-[50vh] flex-1 items-center p-4 text-white flex gap-4 overflow-x-scroll ' >
 
-                    </select> */}
-                </div>
-                <div className='w-100 min-h-[50vh] flex-1 items-center p-4 text-white flex gap-4 overflow-x-scroll ' >
-                    {/* {
-                        episodes?.map((episode) => (
-                            <Card key={episode.id} episodeData={episode} />
-                        ))
-                    } */}
-
-                </div>
             </div>
         </div>
     )
